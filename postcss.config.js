5b3923ac7d3d44098e757c27a0823b1ff550b839
Pierre Laurent <pierrelaurent@macbook-air-de-pierre.home>
2024-11-06 22:07:17 +0100

    update: your changes description

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-preset-env')({
+      features: { 'nesting-rules': false }
+    })
+  ]
+};
