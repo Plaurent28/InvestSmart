f23fba8e2fe2b81237ba421c1de50f53c744fc0f
Pierre Laurent <pierrelaurent@Mac.Home>
2024-11-07 23:13:12 +0100

    pppp

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('postcss-preset-env')({
+      stage: 0,
+    }),
+    // Ajoutez d'autres plugins si nécessaires
+  ],
+};
