43479e3e036cf7409426dcb50ed0cca7a6bef6d9
Pierre Laurent <pierrelaurent@Mac.Home>
2024-11-11 00:44:48 +0000

    oknj

---
+++ src/pages/guide/GuideDemarrageRapide.js
@@ -0,0 +1,14 @@
+// src/pages/guide/GuideDemarrageRapide.js
+
+import React from 'react';
+
+const GuideDemarrageRapide = () => {
+  return (
+    <div>
+      <h1>Guide de Démarrage Rapide</h1>
+      <p>Bienvenue dans le guide de démarrage rapide de InvestSmart.</p>
+    </div>
+  );
+};
+
+export default GuideDemarrageRapide;
